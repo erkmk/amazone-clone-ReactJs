a673fc50fa8aa5b1c59173916a3775a50fa18eb5
Khalid <er.kmk123@gmail.com>
2020-10-15 10:35:32 +0530

    Header done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCh9U5jO8_j_iUxvBnTUHktR6dxMe2KiZY",
+    authDomain: "e-challange-3fa74.firebaseapp.com",
+    databaseURL: "https://e-challange-3fa74.firebaseio.com",
+    projectId: "e-challange-3fa74",
+    storageBucket: "e-challange-3fa74.appspot.com",
+    messagingSenderId: "987383481797",
+    appId: "1:987383481797:web:51e92d4f888c676fc637ac",
+    measurementId: "G-CS5Y6XLN7Y"
+};
