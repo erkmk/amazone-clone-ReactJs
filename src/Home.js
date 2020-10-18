8b2cd80627485871eef7646e252ce33f25857535
Khalid <er.kmk123@gmail.com>
2020-10-18 12:30:32 +0530

    first row prodcut display done at home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./Home.css";
+import Product from './Product';
 
 function Home() {
     return (
         <div className="home">
-            <div className="home_container"></div>
+            <div className="home_container">
+                <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
+
+                <div className="home_row">
+                    {/* Product1 */}
+                    <Product />
+                    <Product />
+                    
+                    {/* Product2 */}
+
+                </div>
+                
+                <div className="home_row">
+                    {/* Product1 */}
+                    {/* product2 */}
+                    {/* product3 */}
+                </div>
+
+                <div className="home_row">
+                    {/* product1 */}
+                </div>
+            </div>
             
         </div>
     )
